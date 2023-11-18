1caa2b02fd3deda3ae9cc07f3183e9dc14c85e17
mozix5 <mosinmd827@gmail.com>
2023-11-19 00:47:16 +0530

    Revert "Update Profile.jsx"
    This reverts commit 9bc4ccf31ba7b8af62b2a6098018c4de45c69d57.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -35,11 +35,10 @@ const Profile = () => {
           backgroundSize: "cover",
         }}
       ></div>
-
-      <div className="flex justify-center">
-        <div className="border-2 sm:w-[90%] ">
+      <div className="flex justify-center bg-fixed bg-slate-400">
+        <div className="border-2 w-[90%] bg-black overflow-x-scroll">
           <div className="flex justify-center gap-4 pr-6 relative -top-6">
-            <div className="rounded-full border-2 border-black max-h-36 w-36 flex">
+            <div className="rounded-full border-2 border-black h-32 w-32 ">
               <img
                 src={data?.user_image}
                 className="object-cover rounded-full h-full w-full"
@@ -47,98 +46,96 @@ const Profile = () => {
               />
             </div>
 
-            <div className="relative top-6">
-              <div className="flex items-center gap-2">
-                <div className="py-3 text-xl font-bold capitalize">
-                  {data?.fullName}
-                </div>
+        <div className="relative top-6">
+          <div className="flex items-center gap-2">
+            <div className="py-3 text-xl font-bold capitalize">
+              {data?.fullName}
+            </div>
 
-                <div className="flex items-center">
-                  <TbDiamondFilled className="text-purple-500 text-xl" />
-                  <MdVerified className="text-green-500 text-lg" />
-                </div>
-              </div>
-
-              <div className="flex gap-3 justify-center">
-                <div>
-                  <div className="border-2 border-slate-400 text-center rounded-md font-bold text-lg text-slate-400 w-20">
-                    {data?.followers}
-                  </div>
-                  <div className="text-sm text-center">followers</div>
-                </div>
-
-                <div>
-                  <div className="border-2 border-slate-400 text-center rounded-md font-bold text-lg text-slate-400 w-20">
-                    {data?.following}
-                  </div>
-                  <div className="text-sm text-center">following</div>
-                </div>
-              </div>
+            <div className="flex items-center">
+              <TbDiamondFilled className="text-purple-500 text-xl" />
+              <MdVerified className="text-green-500 text-lg" />
             </div>
           </div>
+
+          <div className="flex gap-3 justify-center">
+            <div>
+              <div className="border-2 border-slate-400 text-center rounded-md font-bold text-lg text-slate-400 w-20">
+                {data?.followers}
+              </div>
+              <div className="text-sm text-center">followers</div>
+            </div>
+
+            <div>
+              <div className="border-2 border-slate-400 text-center rounded-md font-bold text-lg text-slate-400 w-20">
+                {data?.following}
+              </div>
+              <div className="text-sm text-center">following</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
           <div className="pl-10 mt-1 pb-6">
             <div>{data?.bio}</div>
 
-            <a
-              href={data?.insta_link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-400 py-1"
-            >
-              {data?.insta_link}
-            </a>
+        <a
+          href={data?.insta_link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 py-1"
+        >
+          {data?.insta_link}
+        </a>
 
-            <div className="flex gap-3 items-center py-2">
-              <div className="rounded-full bg-blue-300 p-1 text-white">
-                <FaRegStar />
-              </div>
-              <div className="text-sm">{data?.stats.stars}</div>
-
-              <div className="rounded-full bg-yellow-400 p-1 text-white">
-                <LuThumbsUp />
-              </div>
-              <div className="text-sm">{data?.stats.likes}</div>
-
-              <div className="rounded-full bg-gray-400 p-1 text-white">
-                <FaEye />
-              </div>
-              <div className="text-sm">{data?.stats.views}</div>
-
-              <div className="rounded-full bg-red-500 p-1 text-white">
-                <FaRegHeart />
-              </div>
-              <div className="text-sm">{data?.stats.hearts}</div>
-            </div>
+        <div className="flex gap-3 items-center py-2">
+          <div className="rounded-full bg-blue-300 p-1 text-white">
+            <FaRegStar />
           </div>
+          <div className="text-sm">{data?.stats.stars}</div>
 
-          <div className="pt-4">
-            <Tabs variant="enclosed-colored" colorScheme="yellow">
-              <TabList>
-                <Tab fontWeight="bold" color="#2b8dd8" paddingX="20px">
-                  {data?.posts.length} Posts
-                </Tab>
-              </TabList>
-
-              <TabPanels>
-                <TabPanel>
-                  {data?.posts.map((item, index) => (
-                    <Posts
-                      key={index}
-                      userName={data?.username}
-                      title={item.title}
-                      content={item.content}
-                      type={item.type}
-                      createdAt={item.createdAt}
-                      readAt={item.readAt}
-                      views={item.postViews}
-                    />
-                  ))}
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+          <div className="rounded-full bg-yellow-400 p-1 text-white">
+            <LuThumbsUp />
           </div>
+          <div className="text-sm">{data?.stats.likes}</div>
+
+          <div className="rounded-full bg-gray-400 p-1 text-white">
+            <FaEye />
+          </div>
+          <div className="text-sm">{data?.stats.views}</div>
+
+          <div className="rounded-full bg-red-500 p-1 text-white">
+            <FaRegHeart />
+          </div>
+          <div className="text-sm">{data?.stats.hearts}</div>
         </div>
+      </div>
+
+      <div className="pt-4">
+        <Tabs variant="enclosed-colored" colorScheme="yellow">
+          <TabList>
+            <Tab fontWeight="bold" color="#2b8dd8" paddingX="20px">
+              {data?.posts.length} Posts
+            </Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              {data?.posts.map((item, index) => (
+                <Posts
+                  key={index}
+                  userName={data?.username}
+                  title={item.title}
+                  content={item.content}
+                  type={item.type}
+                  createdAt={item.createdAt}
+                  readAt={item.readAt}
+                  views={item.postViews}
+                />
+              ))}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </div>
     </div>
   );
